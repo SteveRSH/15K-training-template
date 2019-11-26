04fcf1a0be241fa35c53028274e5ad95d0182796
Steve Horlback <stevehorlback@steves-mbp.attlocal.net>
2019-11-26 09:43:52 -0500

    15k running spreadsheet

---
+++ runnning.js
@@ -0,0 +1,262 @@
+//Used javascript to change the data that was inside the cells.
+
+
+//var test = document.getElementById('changeableWeekOne');
+
+function myFunction() {
+  alert(document.getElementById("myTable").rows[1].cells[2].innerHTML);
+}
+
+
+//First week
+
+var firstDay = document.getElementById("myTable").rows[1].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[1].cells[2];
+secondDay.innerHTML = "2 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[1].cells[3];
+thirdDay.innerHTML = "30 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[1].cells[4];
+fourthDay.innerHTML = "2 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[1].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[1].cells[6];
+sixthDay.innerHTML = "2 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[1].cells[7];
+seventhDay.innerHTML = "30 min cross";
+
+
+
+//Second week
+
+var firstDay = document.getElementById("myTable").rows[2].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[2].cells[2];
+secondDay.innerHTML = "3 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[2].cells[3];
+thirdDay.innerHTML = "30 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[2].cells[4];
+fourthDay.innerHTML = "2 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[2].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[2].cells[6];
+sixthDay.innerHTML = "3 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[2].cells[7];
+seventhDay.innerHTML = "30 min cross";
+
+
+
+//Third week
+
+var firstDay = document.getElementById("myTable").rows[3].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[3].cells[2];
+secondDay.innerHTML = "3 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[3].cells[3];
+thirdDay.innerHTML = "35 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[3].cells[4];
+fourthDay.innerHTML = "2 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[3].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[3].cells[6];
+sixthDay.innerHTML = "4 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[3].cells[7];
+seventhDay.innerHTML = "30 min cross";
+
+
+
+//Fourth week
+
+var firstDay = document.getElementById("myTable").rows[4].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[4].cells[2];
+secondDay.innerHTML = "2 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[4].cells[3];
+thirdDay.innerHTML = "35 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[4].cells[4];
+fourthDay.innerHTML = "2 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[4].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[4].cells[6];
+sixthDay.innerHTML = "2 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[4].cells[7];
+seventhDay.innerHTML = "40 min cross";
+
+
+
+//Fifth week
+
+var firstDay = document.getElementById("myTable").rows[5].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[5].cells[2];
+secondDay.innerHTML = "4 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[5].cells[3];
+thirdDay.innerHTML = "40 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[5].cells[4];
+fourthDay.innerHTML = "3 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[5].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[5].cells[6];
+sixthDay.innerHTML = "5 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[5].cells[7];
+seventhDay.innerHTML = "40 min cross";
+
+
+
+//Sixth week
+
+var firstDay = document.getElementById("myTable").rows[6].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[6].cells[2];
+secondDay.innerHTML = "4 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[6].cells[3];
+thirdDay.innerHTML = "40 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[6].cells[4];
+fourthDay.innerHTML = "3 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[6].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[6].cells[6];
+sixthDay.innerHTML = "6 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[6].cells[7];
+seventhDay.innerHTML = "50 min cross";
+
+
+
+//Seventh week
+
+var firstDay = document.getElementById("myTable").rows[7].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[7].cells[2];
+secondDay.innerHTML = "3 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[7].cells[3];
+thirdDay.innerHTML = "45 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[7].cells[4];
+fourthDay.innerHTML = "3 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[7].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[7].cells[6];
+sixthDay.innerHTML = "4 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[7].cells[7];
+seventhDay.innerHTML = "50 min cross";
+
+
+
+//Eighth week
+
+var firstDay = document.getElementById("myTable").rows[8].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[8].cells[2];
+secondDay.innerHTML = "5 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[8].cells[3];
+thirdDay.innerHTML = "45 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[8].cells[4];
+fourthDay.innerHTML = "3 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[8].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[8].cells[6];
+sixthDay.innerHTML = "7 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[8].cells[7];
+seventhDay.innerHTML = "60 min cross";
+
+
+
+//Ninth week
+
+var firstDay = document.getElementById("myTable").rows[9].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[9].cells[2];
+secondDay.innerHTML = "5 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[9].cells[3];
+thirdDay.innerHTML = "45 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[9].cells[4];
+fourthDay.innerHTML = "3 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[9].cells[5];
+fifthDay.innerHTML = "Rest";
+
+var sixthDay = document.getElementById("myTable").rows[9].cells[6];
+sixthDay.innerHTML = "8 mile run";
+
+var seventhDay = document.getElementById("myTable").rows[9].cells[7];
+seventhDay.innerHTML = "60 min cross";
+
+
+
+//Tenth week
+
+var firstDay = document.getElementById("myTable").rows[10].cells[1];
+firstDay.innerHTML = "Stretch & strengthen";
+
+var secondDay = document.getElementById("myTable").rows[10].cells[2];
+secondDay.innerHTML = "3 mile run";
+
+var thirdDay = document.getElementById("myTable").rows[10].cells[3];
+thirdDay.innerHTML = "30 min cross";
+
+var fourthDay = document.getElementById("myTable").rows[10].cells[4];
+fourthDay.innerHTML = "2 mile run + strength";
+
+var fifthDay = document.getElementById("myTable").rows[10].cells[5];
+fifthDay.innerHTML = "2 mile run or rest";
+
+var sixthDay = document.getElementById("myTable").rows[10].cells[6];
+sixthDay.innerHTML = "Rest";
+
+var seventhDay = document.getElementById("myTable").rows[10].cells[7];
+seventhDay.innerHTML = "15K Race";
+
+
+
+
+
